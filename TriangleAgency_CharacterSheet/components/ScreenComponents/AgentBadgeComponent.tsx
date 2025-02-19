5006b40ee0f7fc3572ef30203393b75d1e5df88d
Anna Karwacki <avkarwacki@gmail.com>
2025-02-19 14:40:22 -0500

    visibility button for badge
    also remembrance of me choking during my interview today.
    
    remember to be better

--- TriangleAgency_CharacterSheet/components/ScreenComponents/AgentBadgeComponent.tsx
+++ TriangleAgency_CharacterSheet/components/ScreenComponents/AgentBadgeComponent.tsx
@@ -1,19 +1,20 @@
 import { StyleSheet } from 'react-native';
 
 import { Text, View } from '@/components/Themed';
-import React = require('react');
 import ImageUpload from './helpers/imageUpload';
 import { Button, Card, CardContent, TextField } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import { setNamePronouns } from '@/redux/reducers/UserInfo';
 import { FontAwesome } from '@expo/vector-icons';
 import { Image } from 'react-native';
+import React from 'react';
 
 export default function AgentBadgeComponent() { 
 
   const [editMode, setEditMode] = React.useState(false)
   const [nameInput, setNameInput] = React.useState('');
   const [pronounInput, setPronounInput] = React.useState('');
+  const [visble, setVisible] = React.useState(true);
   const dispatch = useDispatch();
 
   const handleNameInputChange = (event:any) => {
@@ -27,10 +28,34 @@ export default function AgentBadgeComponent() {
       dispatch(setNamePronouns({name:nameInput, pronouns: pronounInput}));
     }
 
+  /* 2/19/2025 a remeberance of failure
+  function balanceCheck(balance: string){
+    let balanceArr = balance.split('');
+    let cleanedArr = balanceArr.filter((value: string)=> {return value === '(' || value === ')'});
+    if(cleanedArr.length === 0 ){
+      return true;
+    }
+    if(cleanedArr.length % 2 === 1){
+      return false;
+    }
+    for(let x = 0; x < cleanedArr.length/2; x++){
+      if(cleanedArr[x] !== '(' || cleanedArr[cleanedArr.length - x -1 ] !== ')'){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  console.log('checking my own fuck dumb self: ' + balanceCheck('asd()'));
+  console.log('checking my own fuck dumb self: ' + balanceCheck('asd(()('));
+  console.log('checking my own fuck dumb self: ' + balanceCheck('asd(()'));
+  console.log('checking my own fuck dumb self: ' + balanceCheck('asd('));
+  console.log('checking my own fuck dumb self: ' + balanceCheck('asd()()'));
+*/
   return (
     <Card style={{width: '70%', margin: 15}}>
         <CardContent>
-            <View style={styles.container}>
+            {visble && <View style={styles.container}>
                 <Image source={require('../../assets/images/TALogo.png')} style={{height: 60, width: 160, marginBottom:15}}/>
                 <View style={{marginBottom: 15}}>
                     <ImageUpload />
@@ -47,9 +72,8 @@ export default function AgentBadgeComponent() {
                   <Text style={styles.bodyText}>{pronounInput}</Text>
                   <Button variant="outlined" style={styles.button}  onClick={() => { setEditMode(true);}}>Edit</Button>
                 </View>}
-                
-                
-            </View>
+            </View>}
+            <Button style={{width:'100%'}}  onClick={() => { setVisible(!visble);}}>{visble? 'Hide' : 'Show'} Agent Badge</Button>
         </CardContent>
     </Card>
   );
